23fb13d56d285e302033ee297e5c2a4b95997879
Matheus Barbosa <matheusbs.silva@outlook.com>
2026-06-09 20:32:21 -0300

    Implementing controller to register user

---
+++ src/CashFlow.Application/UseCases/User/PasswordValidator.cs
@@ -0,0 +1,72 @@
+﻿using CashFlow.Exception.ErrorMessages;
+using FluentValidation;
+using FluentValidation.Validators;
+using System.Text.RegularExpressions;
+
+namespace CashFlow.Application.UseCases.User;
+
+public partial class PasswordValidator<T> : PropertyValidator<T, string>
+{
+    private const ushort MINIMUM_LENGTH = 8;
+    private const string ERROR_MESSAGE_KEY = "ErrorMessage";
+
+    public override string Name => "PasswordValidator";
+
+    protected override string GetDefaultMessageTemplate(string errorCode)
+    {
+        return $"{{{ERROR_MESSAGE_KEY}}}";
+    }
+
+    public override bool IsValid(ValidationContext<T> context, string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+        if (password.Length < MINIMUM_LENGTH)
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+        if (!UpperCaseLetter().IsMatch(password))
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+        
+        if (!LowerCaseLetter().IsMatch(password))
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+        
+        if (!Number().IsMatch(password))
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+        
+        if (!SpecialSymbols().IsMatch(password))
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessages.INVALID_PASSWORD);
+            return false;
+        }
+
+        return true;
+    }
+
+    [GeneratedRegex(@"[A-Z]+")]
+    private static partial Regex UpperCaseLetter();
+    
+    [GeneratedRegex(@"[a-z]+")]
+    private static partial Regex LowerCaseLetter();
+    
+    [GeneratedRegex(@"[0-9]+")]
+    private static partial Regex Number();
+    
+    [GeneratedRegex(@"[\!\?\*\.]+")]
+    private static partial Regex SpecialSymbols();
+}
---
+++ src/CashFlow.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception.ErrorMessages;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.User.Register;
+
+public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+{
+    public RegisterUserValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_CANNOT_BE_EMPTY);
+
+        RuleFor(user => user.Email)
+            .NotEmpty().WithMessage(ResourceErrorMessages.EMAIL_CANNOT_BE_EMPTY)
+            .EmailAddress().WithMessage(ResourceErrorMessages.EMAIL_INVALID);
+
+        RuleFor(user => user.Password).SetValidator(new PasswordValidator<RequestRegisterUserJson>());
+    }
+}
