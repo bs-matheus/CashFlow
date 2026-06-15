f842e1cdd96d36c5d287d7989619cb21793bbfb5
Matheus Barbosa <matheusbs.silva@outlook.com>
2026-06-15 16:53:39 -0300

    Add unit tests for User validators

---
+++ tests/Validators.Tests/Users/PasswordValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Application.UseCases.Users;
+using CashFlow.Communication.Requests;
+using FluentValidation;
+using Shouldly;
+
+namespace Validators.Tests.Users;
+
+public class PasswordValidatorTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    [InlineData("a")]
+    [InlineData("aa")]
+    [InlineData("aaa")]
+    [InlineData("aaaa")]
+    [InlineData("aaaaa")]
+    [InlineData("aaaaaa")]
+    [InlineData("aaaaaaa")]
+    [InlineData("aaaaaaaa")]
+    [InlineData("AAAAAAAA")]
+    [InlineData("Aaaaaaaa")]
+    [InlineData("Aaaaaaa1")]
+    public void Error_Password_Invalid(string password)
+    {
+        //Arrange
+        var validator = new PasswordValidator<RequestRegisterUserJson>();
+        var context = new ValidationContext<RequestRegisterUserJson>(new RequestRegisterUserJson());
+
+        //Act
+        var result = validator.IsValid(context, password);
+
+        //Assert
+        result.ShouldBeFalse();
+    }
+}
---
+++ tests/Validators.Tests/Users/Register/RegisterUserValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Exception.ErrorMessages;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace Validators.Tests.Users.Register;
+
+public class RegisterUserValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public void Error_Name_Empty(string name)
+    {
+        //Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = name;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            c => c.ShouldHaveSingleItem(),
+            c => c.ShouldContain(error => error.ErrorMessage.Equals(ResourceErrorMessages.NAME_CANNOT_BE_EMPTY)));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public void Error_Email_Empty(string email)
+    {
+        //Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Email = email;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            c => c.ShouldHaveSingleItem(),
+            c => c.ShouldContain(error => error.ErrorMessage.Equals(ResourceErrorMessages.EMAIL_CANNOT_BE_EMPTY)));
+    }
+
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        //Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Email = "user.com";
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            c => c.ShouldHaveSingleItem(),
+            c => c.ShouldContain(error => error.ErrorMessage.Equals(ResourceErrorMessages.EMAIL_INVALID)));
+    }
+    
+    [Fact]
+    public void Error_Password_Empty()
+    {
+        //Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Password = string.Empty;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            c => c.ShouldHaveSingleItem(),
+            c => c.ShouldContain(error => error.ErrorMessage.Equals(ResourceErrorMessages.INVALID_PASSWORD)));
+    }
+}
